db748d76a54ef7fe39dca3b5e7bfafb408c80623
evangilang <evangilang96@email.unikom.ac.id>
2018-01-29 14:21:17 +0700

    anjay

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -27,6 +27,7 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("viewWillAppear called")
+        print("anjay ukhti")
     }
 
 
